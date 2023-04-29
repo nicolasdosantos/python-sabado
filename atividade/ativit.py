23cc2fe37a69a3cd4669dd861ad2687b44bc4c26
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-04-29 16:55:49 -0300

    Update ativit.py

--- atividade/ativit.py
+++ atividade/ativit.py
@@ -26,8 +26,50 @@ minimo.append(minimos)
 for i in range(len(produtos)):
     print(f"A quatidade de {produtos[i]} é {estoque[i]} e seu estoque minimo é de {minimo[i]}")
 
-# ATRIBUINDO LIMITE DO ESTOQUE 
+# MOSTRANDO SE A QUANTIDADE DE PRODUTOS É MENOR 
 
 for i in range(len(produtos)):
     if estoque[i] <= minimo[i]:
-        print(f"\nProduto{produtos[i]} com a quantidade minima")
+        print(f"Produto{produtos[i]} com a quantidade minima")
+
+# LISTA DENTRO DE UMA LISTA, PARA COLOCAR AS INFORMAÇÕES EM UMA SÓ
+
+lista = []
+for i in range(len(produtos)):
+    lista.append([produtos[i],estoque[i],minimo[i]])
+print(lista)
+
+# USUARIO REMOVE ITEM DA LISTA 
+
+while True:
+    remove = input("Digite o produto que voce quer apagar produto aqui: ")
+    if remove in produtos:
+        break
+    else:
+        print("Produto invalido: ")
+    ind = produtos.index(remove)
+    produtos.remove(remove)
+    estoque.remove(estoque[ind])
+    minimo.remove(minimo[ind])
+    print(produtos,estoque,minimo)
+
+# PARTE DA COMPRA DE PRODUTO
+
+compras = []
+while True:
+    compra_produto = input("Digite o seu produto aqui: ")
+    compra_quantidade = int(input("Informe a quantidade de produtos:"))
+    compras.append([compra_produto, compra_quantidade])
+    repete = input("Deseja continuar sua compra?S ou N: ")
+    print(repete)
+    if repete.upper() == "N" or "n":
+        break
+print(compras)
+
+# COMPARAÇÃO COM O PEDIDO E O ESTOQUE
+
+for itens in compras:
+  if itens[0] not in produtos: 
+    print(f"Produto {itens[0]} não tem")
+  elif itens[1] > estoque[produtos.index(itens[0])]:
+    print('Saldo insuficiente de {itens[0]}')
